b6861bb9cf0c35a67fb5f861bf70b75ce41a1e58
wxquare <wxquare@163.com>
2020-11-09 20:52:50 +0800

    Update goroutine_example1.go

--- golang/learn_golang/goroutine_example1.go
+++ golang/learn_golang/goroutine_example1.go
@@ -2,31 +2,47 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-var chan1 chan int
-var chan2 chan int
-
-func foo1() {
-	for i := 0; i < 10; i++ {
-		chan1 <- 2*i + 1
-		fmt.Println(<-chan2)
-	}
-}
-
-func foo2() {
-	for i := 1; i <= 10; i++ {
-		fmt.Println(<-chan1)
-		chan2 <- 2 * i
-	}
-}
+const goroutineNum = 7
+const maxVal = 100
 
 func main() {
-	chan1 = make(chan int)
-	chan2 = make(chan int)
-	go foo1()
-	go foo2()
 
-	time.Sleep(3 * time.Second)
+	chans := make([]chan int, goroutineNum)
+	for i := 0; i < goroutineNum; i++ {
+		chans[i] = make(chan int, 1)
+	}
+
+	// exitChan
+	exitChan := make(chan int)
+
+	cur := 1
+
+	for i := 0; i < goroutineNum; i++ {
+		go func(i int) {
+			for {
+				<-chans[i]
+				if cur > maxVal {
+					exitChan <- 1
+					break
+				}
+				fmt.Printf("gountine = %d,cur = %d\n", i, cur)
+				cur = cur + 1
+				next := i + 1
+				if next >= goroutineNum {
+					next = 0
+				}
+				chans[next] <- 1
+			}
+		}(i)
+	}
+
+	chans[0] <- 1 // start
+
+	select {
+	case <-exitChan:
+		fmt.Println("end")
+	}
+
 }
